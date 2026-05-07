a7d7bc9a907cc0d5416411a4367a53e9a9647642
spiderpilo <piolo.patag@gmail.com>
2026-05-06 21:35:14 -0700

    Update pipeline and training scripts

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
-DEFAULT_LOCATION = "Long Beach, CA"
-DEFAULT_CATEGORY = "restaurants"
-MAX_RESULTS = 20
